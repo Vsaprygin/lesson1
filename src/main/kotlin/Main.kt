142998011df2caf5e01125646bd7d0b64fda8e54
Виталий Сапрыгин <saprygin@altasib.ru>
2022-11-25 23:14:49 +0700

    result

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 fun main() {
-    println(message = "Hello World!")
-
+    println("Объем паралепипеда")
+    print("Введите длину ")
+    val length = readln()
+    print("Введите ширину ")
+    val width = readln()
+    print("Введите высоту ")
+    val height = readln()
+    var result = length.toInt() * width.toInt() * height.toInt()
+    print("Объем параллепипеда равен $result")
 }
